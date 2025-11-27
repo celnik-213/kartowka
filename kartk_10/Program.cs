8dc44ce9acb0b3e5ab44a894dce262b6b1108a87
celnik-213 <uT_4000@zsp3zamosc.pl>
2025-11-27 09:51:38 +0100

    zadania 10

---
+++ kartk_10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace kartk_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dzien = 15, miesiac = 11, rok = 2024;
+
+            DateTime data = new DateTime(rok, miesiac, dzien);
+
+            string dzienTygodnia = data.DayOfWeek.ToString();
+
+            Console.WriteLine($"Dzień tygodnia: {dzienTygodnia}");
+        }
+    }
+}
